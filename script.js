e503d1525a7ff50df928e058236404b8685b9a97
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-05-18 18:43:09 -0700

    func that sets each position and transformation of each arm

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ let hrPosition = (hr * 360) / 12 + (min * (360 / 60)) / 12;
 let minPosition = (min * 360) / 60 + (sec * (360 / 60)) / 60;
 let secPosition = (sec * 360) / 60;
 
+//func that runs the clock every
+const runClock = () => {
+    //set each position when the function is called 
+    hrPosition = hrPosition + 3 / 360;
+    minPosition = minPosition + 6 / 60;
+    secPosition = secPosition + 6;
+
+    //set the transformation for each arm
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition +"deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+};
+
+
+setInterval(runClock, 1000);
